6f31ce5dca5b05444aadbb5ea4fdcf5000731b06
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-02 16:32:44 -0500

    Add Method To Translate English To Pig Latin
    Add Method to translate English word to Pig Latin for Pig Lation Project

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,15 @@
+def translate(text)
+	vowel = ["a", "e", "i", "o", "u"]
+	pig_latin = ""
+
+	text.each_char.with_index do |char, index| 
+		if vowel.include?(char) 
+			pig_latin = text[index...text.length] << pig_latin
+			break
+		else
+			pig_latin += char
+		end
+	end
+
+	"#{pig_latin}ay"
+end
