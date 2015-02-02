f2fc3a5fd3c6591f7b7828d4fd44a5a49e2620ae
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-02 14:49:52 -0500

    Add Repeat Method To Repeat Text In Simon Says
    Add in Repeat method to repeat text a number of times to the screen in
    the Simon Says Project.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -5,3 +5,11 @@ end
 def shout(say_something)
 	say_something.upcase
 end
+
+def repeat(say_something, num = 1)
+	return_text = num == 1 ? say_something : ""
+
+	num.times { return_text += " #{say_something}" }
+
+	return_text.strip
+end
