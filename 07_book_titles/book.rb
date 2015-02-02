a8a24bdbfa30d3bfc16652c84e2717026cce07ac
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-02 18:47:43 -0500

    Add Class Book & Reused Method Titleize
    Add in a class Book and then reused the Titleize method from Simon Says
    Project as it is the same function.

---
+++ 07_book_titles/book.rb
@@ -0,0 +1,13 @@
+class Book
+	def titleize(text)
+		non_capitalize_words = ["and", "over", "the", "a", "an", "in", "of"]
+
+		capitalize_words = text.split(" ").map(&:capitalize)
+
+		capitalize_words.map!.with_index do |word, index|
+			non_capitalize_words.index(word.downcase) == nil || index == 0 ? word : word.downcase
+		end
+
+		capitalize_words.join(" ")
+	end
+end
