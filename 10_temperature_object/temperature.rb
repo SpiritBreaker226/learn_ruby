14787a767aa364c4f27a0f9152fcef363aaa7b47
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-04 10:01:13 -0500

    Add to_celsius() Convert Fahrenheit To Celsius
    Add in to_celsius() which will convert fahrenheit to celsius in the
    Temperature project.

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -7,4 +7,8 @@ class Temperature
 	def to_fahrenheit
 			@celsius == nil ? @fahrenheit : (@celsius * 1.8) + 32
 	end
+
+	def to_celsius
+			@fahrenheit == nil ? @celsius : (@fahrenheit - 32) * 5/9
+	end
 end
