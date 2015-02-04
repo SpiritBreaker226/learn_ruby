82acd391730d5e4bcf2071857d99ba12df939eb7
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-04 09:53:40 -0500

    Add to_fahrenheit() Convert Celsius To Fahrenheit
    Add in to_fahrenheit() which will convert celsius to fahrenheit in the
    Temperature project.

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,8 +1,10 @@
 class Temperature
 	def initialize(optional = {})
-		@fahrenheit = optional[:f] || 0 
-		@celsius = optional[:c] || 0
+		@fahrenheit = optional[:f]
+		@celsius = optional[:c]
 	end
 	
-	
+	def to_fahrenheit
+			@celsius == nil ? @fahrenheit : (@celsius * 1.8) + 32
+	end
 end
